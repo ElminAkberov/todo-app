c3428f000140de0bfbe84b2dd85fd3c01b6afd1c
Elmin Akbarov <elmin_akbarov+epam@epam.com>
2026-07-28 10:25:56 +0400

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// @ts-ignore
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />,
+);
